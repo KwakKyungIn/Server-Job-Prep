6761101d8056039bcf24a28df531ca8f98a19da8
kwak <b_mule@naver.com>
2025-08-15 19:59:11 +0900

    C++ : lock과 lock guard 기초 부분 공부 실습

--- C++/Cpp_Server/Server/GameServer/GameServer.cpp
+++ C++/Cpp_Server/Server/GameServer/GameServer.cpp
@@ -4,32 +4,48 @@
 #include <atomic> // atomic은 멀티스레드 환경에서 안전하게 변수를 공유하기 위해 사용합니다.
 
 #include <thread> //윈도우즈를 가져오면 리눅스 환경에서는 안될수도있으니까
+#include <mutex>	 // mutex는 스레드 간의 동기화를 위해 사용합니다.
+vector<int32> v;
+mutex m; // 벡터에 접근할 때 사용할 뮤텍스입니다.
 
-atomic<int32> sum = 0;
-void Add()
+template<typename T>
+class LockGuard
 {
-	for (int32 i = 0; i < 1000000; ++i)
+	public:
+		LockGuard(T& m)
+		{
+			_mtx = m; // 생성자에서 뮤텍스를 잠급니다.
+			_mtx-> lock(); // 뮤텍스를 잠급니다.
+		}
+		
+	~LockGuard() 
+	{ 
+		_mtx->unlock();// 소멸자에서 뮤텍스를 해제합니다.
+
+	} 
+
+	private:
+	T& _mtx; // 뮤텍스 참조
+};
+
+void Push()
+{
+	for (int32 i = 0; i < 10000; ++i)
 	{
-		sum += i;
+		LockGuard<mutex> lockguard(m); // LockGuard를 사용하여 뮤텍스를 잠급니다.
+		v.push_back(i);
+
 	}
 }
-void Sub()
-{
-	for (int32 i = 0; i < 1000000; ++i)
-	{
-		sum -= i;
-	}
-}
+
 
 int main()
 {
-	Add();
-	Sub();
-	cout << "Sum: " << sum << endl;
+	v.reserve(20000); // 벡터의 크기를 미리 예약합니다. 이로 인해 동적 할당이 줄어들어 성능이 향상됩니다.
+	thread t1(Push);
+	thread t2(Push);
+	t1.join(); // t1 스레드가 종료될 때까지 기다립니다.
+	t2.join(); // t2 스레드가 종료될 때까지 기다립니다.
+	cout << "Size of vector: " << v.size() << endl;
 
-	std::thread t1(Add);
-	std::thread t2(Sub);
-	t1.join();
-	t2.join();
-	cout << "Final Sum: " << sum << std::endl;
 }
