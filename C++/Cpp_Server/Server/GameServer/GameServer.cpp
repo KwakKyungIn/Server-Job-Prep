25b736d1bd3644fc01e9d0a1d0e562247b868769
kwak <b_mule@naver.com>
2025-08-17 15:00:57 +0900

    C++ : 스핀락 오류버전 실습

--- C++/Cpp_Server/Server/GameServer/GameServer.cpp
+++ C++/Cpp_Server/Server/GameServer/GameServer.cpp
@@ -5,47 +5,59 @@
 
 #include <thread> //윈도우즈를 가져오면 리눅스 환경에서는 안될수도있으니까
 #include <mutex>	 // mutex는 스레드 간의 동기화를 위해 사용합니다.
-vector<int32> v;
+
 mutex m; // 벡터에 접근할 때 사용할 뮤텍스입니다.
+int32 sum = 0;
 
-template<typename T>
-class LockGuard
+class SpinLock
 {
-	public:
-		LockGuard(T& m)
+public:
+	void lock()
+	{
+		while (_locked)
 		{
-			_mtx = m; // 생성자에서 뮤텍스를 잠급니다.
-			_mtx-> lock(); // 뮤텍스를 잠급니다.
+
 		}
-		
-	~LockGuard() 
-	{ 
-		_mtx->unlock();// 소멸자에서 뮤텍스를 해제합니다.
 
-	} 
+		_locked = true; // 스핀락을 획득합니다.
+	}
+	void unlock()
+	{
+		_locked = false; // 스핀락을 해제합니다.
+	}
 
-	private:
-	T& _mtx; // 뮤텍스 참조
+
+private:
+	bool _locked = false;
 };
 
-void Push()
-{
-	for (int32 i = 0; i < 10000; ++i)
-	{
-		LockGuard<mutex> lockguard(m); // LockGuard를 사용하여 뮤텍스를 잠급니다.
-		v.push_back(i);
+SpinLock spinLock; // 스핀락 객체 생성
 
+void Add()
+{
+	for(int32 i = 0; i < 1000000; ++i)
+	{
+		lock_guard<SpinLock> guard(spinLock); 
+		sum++;
 	}
 }
 
+void Sub()
+{
+	for (int32 i = 0; i < 1000000; ++i)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
+	}
+}
 
 int main()
 {
-	v.reserve(20000); // 벡터의 크기를 미리 예약합니다. 이로 인해 동적 할당이 줄어들어 성능이 향상됩니다.
-	thread t1(Push);
-	thread t2(Push);
+	thread t1(Add);
+	thread t2(Sub);
+	
 	t1.join(); // t1 스레드가 종료될 때까지 기다립니다.
 	t2.join(); // t2 스레드가 종료될 때까지 기다립니다.
-	cout << "Size of vector: " << v.size() << endl;
 
+	cout << "Final sum: " << sum << endl; // 최종 합계를 출력합니다.
 }
