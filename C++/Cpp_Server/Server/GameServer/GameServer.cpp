18b2242b19c1806629eae587598aa872884f4bc7
kwak <b_mule@naver.com>
2025-08-17 16:15:52 +0900

    C++ : Event 사용 실습

--- C++/Cpp_Server/Server/GameServer/GameServer.cpp
+++ C++/Cpp_Server/Server/GameServer/GameServer.cpp
@@ -2,70 +2,60 @@
 #include <iostream>
 #include "CorePch.h"
 #include <atomic> // atomic은 멀티스레드 환경에서 안전하게 변수를 공유하기 위해 사용합니다.
-
 #include <thread> //윈도우즈를 가져오면 리눅스 환경에서는 안될수도있으니까
 #include <mutex>	 // mutex는 스레드 간의 동기화를 위해 사용합니다.
-mutex m; // 벡터에 접근할 때 사용할 뮤텍스입니다.
-int32 sum = 0;
+#include <windows.h> // Windows API를 사용하기 위해 포함합니다.
 
-class SpinLock
+mutex m;
+queue<int32> q;
+HANDLE handle;
+
+void Producer()
 {
-public:
-	void lock()
+	while (true)
 	{
-		//cas(Compare and Swap) 알고리즘을 사용하여 스핀락을 구현합니다.
-		bool expected = false; // 스핀락이 잠겨있지 않은 상태를 나타냅니다.
-		bool desired = true; // 스핀락을 잠글 상태를 나타냅니다.
-		// compare_exchange_strong은 _locked가 expected와 같으면 desired로 변경하고 true를 반환합니다.
-		// 그렇지 않으면 _locked의 현재 값을 expected에 저장하고 false를 반환합니다.
-
-	
-		while (_locked.compare_exchange_strong(expected, desired)==false)
 		{
-			expected = false; // expected를 false로 초기화합니다.
-
-			this_thread::sleep_for(chrono::microseconds(100));  //스핀락을 획득하지 못했을 때 잠시 대기합니다.
-		
+			unique_lock<mutex> lock(m); // mutex를 잠급니다.
+			q.push(100); // 큐에 데이터를 추가합니다.
 		}
 
-	}
-	void unlock()
-	{
-		_locked.store(false); // 스핀락을 해제합니다.
-	}
-
-
-private:
-	atomic<bool> _locked = false;
-};
-
-SpinLock spinLock; // 스핀락 객체 생성
-
-void Add()
-{
-	for(int32 i = 0; i < 1000000; ++i)
-	{
-		lock_guard<SpinLock> guard(spinLock); 
-		sum++;
+		::SetEvent(handle); // 이벤트를 설정합니다. (이벤트를 신호 상태로 변경합니다.)
+		this_thread::sleep_for(std::chrono::milliseconds(1000)); // 100ms 대기합니다.
 	}
 }
 
-void Sub()
+void Consumer()
 {
-	for (int32 i = 0; i < 1000000; ++i)
+	while (true)
 	{
-		lock_guard<SpinLock> guard(spinLock);
-		sum--;
+		{
+			::WaitForSingleObject(handle, INFINITE); // 이벤트가 신호 상태가 될 때까지 대기합니다.
+
+			unique_lock<mutex> lock(m); // mutex를 잠급니다.
+			if(q.empty()==false) // 큐가 비어있지 않으면
+			{
+				int32 data = q.front(); // 큐의 앞에서 데이터를 가져옵니다.
+				q.pop(); // 큐에서 데이터를 제거합니다.
+				cout << "Consumed: " << data << endl; // 소비된 데이터를 출력합니다.
+			}
+		}
 	}
 }
 
 int main()
 {
-	thread t1(Add);
-	thread t2(Sub);
-	
-	t1.join(); // t1 스레드가 종료될 때까지 기다립니다.
-	t2.join(); // t2 스레드가 종료될 때까지 기다립니다.
+	//커널 오브젝트
+	//usage count
+	//signal, non-signal << bool
+	//auto manual
+	handle = ::CreateEvent(NULL/*보안 속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL/*name*/); // 이벤트를 생성합니다. 
 
-	cout << "Final sum: " << sum << endl; // 최종 합계를 출력합니다.
+
+	thread t1(Producer); // 생산자 스레드를 생성합니다.
+	thread t2(Consumer); // 소비자 스레드를 생성합니다.
+	t1.join(); // 생산자 스레드가 종료될 때까지 기다립니다.
+	t2.join(); // 소비자 스레드가 종료될 때까지 기다립니다.
+
+	::CloseHandle(handle); // 이벤트 핸들을 닫습니다.
+
 }
