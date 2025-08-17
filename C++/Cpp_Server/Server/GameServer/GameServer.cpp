ca176e9b1c9a65bc87c760ee52a92b56e8a506fb
kwak <b_mule@naver.com>
2025-08-17 15:18:41 +0900

    C++ : SpinLock 성공버전 실습

--- C++/Cpp_Server/Server/GameServer/GameServer.cpp
+++ C++/Cpp_Server/Server/GameServer/GameServer.cpp
@@ -5,7 +5,6 @@
 
 #include <thread> //윈도우즈를 가져오면 리눅스 환경에서는 안될수도있으니까
 #include <mutex>	 // mutex는 스레드 간의 동기화를 위해 사용합니다.
-
 mutex m; // 벡터에 접근할 때 사용할 뮤텍스입니다.
 int32 sum = 0;
 
@@ -14,21 +13,27 @@ class SpinLock
 public:
 	void lock()
 	{
-		while (_locked)
-		{
+		//cas(Compare and Swap) 알고리즘을 사용하여 스핀락을 구현합니다.
+		bool expected = false; // 스핀락이 잠겨있지 않은 상태를 나타냅니다.
+		bool desired = true; // 스핀락을 잠글 상태를 나타냅니다.
+		// compare_exchange_strong은 _locked가 expected와 같으면 desired로 변경하고 true를 반환합니다.
+		// 그렇지 않으면 _locked의 현재 값을 expected에 저장하고 false를 반환합니다.
 
+	
+		while (_locked.compare_exchange_strong(expected, desired)==false)
+		{
+			expected = false; // expected를 false로 초기화합니다.
 		}
 
-		_locked = true; // 스핀락을 획득합니다.
 	}
 	void unlock()
 	{
-		_locked = false; // 스핀락을 해제합니다.
+		_locked.store(false); // 스핀락을 해제합니다.
 	}
 
 
 private:
-	bool _locked = false;
+	atomic<bool> _locked = false;
 };
 
 SpinLock spinLock; // 스핀락 객체 생성
