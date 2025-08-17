ac1a4b697b7697d0048f22d027866036720b9095
kwak <b_mule@naver.com>
2025-08-17 19:39:59 +0900

    C++ : Condition Variable 부분 실습

--- C++/Cpp_Server/Server/GameServer/GameServer.cpp
+++ C++/Cpp_Server/Server/GameServer/GameServer.cpp
@@ -8,7 +8,7 @@
 
 mutex m;
 queue<int32> q;
-HANDLE handle;
+condition_variable cv; // condition_variable은 스레드 간의 통신을 위해 사용합니다.
 
 void Producer()
 {
@@ -19,8 +19,8 @@ void Producer()
 			q.push(100); // 큐에 데이터를 추가합니다.
 		}
 
-		::SetEvent(handle); // 이벤트를 설정합니다. (이벤트를 신호 상태로 변경합니다.)
-		this_thread::sleep_for(std::chrono::milliseconds(1000)); // 100ms 대기합니다.
+		cv.notify_one(); // 대기 중인 소비자 스레드에게 알립니다.
+		//this_thread::sleep_for(std::chrono::milliseconds(1000)); // 100ms 대기합니다.
 	}
 }
 
@@ -29,10 +29,12 @@ void Consumer()
 	while (true)
 	{
 		{
-			::WaitForSingleObject(handle, INFINITE); // 이벤트가 신호 상태가 될 때까지 대기합니다.
+		
+
 
 			unique_lock<mutex> lock(m); // mutex를 잠급니다.
-			if(q.empty()==false) // 큐가 비어있지 않으면
+			cv.wait(lock, [] { return !q.empty(); }); // 큐가 비어있지 않을 때까지 대기합니다.
+			
 			{
 				int32 data = q.front(); // 큐의 앞에서 데이터를 가져옵니다.
 				q.pop(); // 큐에서 데이터를 제거합니다.
@@ -47,8 +49,7 @@ int main()
 	//커널 오브젝트
 	//usage count
 	//signal, non-signal << bool
-	//auto manual
-	handle = ::CreateEvent(NULL/*보안 속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL/*name*/); // 이벤트를 생성합니다. 
+	//auto manual 
 
 
 	thread t1(Producer); // 생산자 스레드를 생성합니다.
@@ -56,6 +57,5 @@ int main()
 	t1.join(); // 생산자 스레드가 종료될 때까지 기다립니다.
 	t2.join(); // 소비자 스레드가 종료될 때까지 기다립니다.
 
-	::CloseHandle(handle); // 이벤트 핸들을 닫습니다.
 
 }
