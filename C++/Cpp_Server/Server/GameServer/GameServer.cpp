dae498f565c7208bcb09b4dd4851fb17e0f78517
kwak <b_mule@naver.com>
2025-08-20 12:49:23 +0900

    C++ : 실습 파일 삭제

--- C++/Cpp_Server/Server/GameServer/GameServer.cpp
+++ C++/Cpp_Server/Server/GameServer/GameServer.cpp
@@ -7,30 +7,8 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
 
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(chrono::milliseconds(100));
-			}
-		});
 
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(chrono::milliseconds(100));
-			}
-		});
-
-	GThreadManager->Join();
 }
