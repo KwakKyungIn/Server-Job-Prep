5a42ed6e2ae5c05a97fcf5ee4c0ba8eb7b536b64
kwak <b_mule@naver.com>
2025-08-20 17:59:50 +0900

    C++ : StompAllocator 실습

--- C++/Cpp_Server/Server/ServerCore/Allocator.h
+++ C++/Cpp_Server/Server/ServerCore/Allocator.h
@@ -7,7 +7,19 @@
 class BaseAllocator
 {
 public:
-	static void* Alloc(int32 size);
+	static void*	Alloc(int32 size);
 	static void		Release(void* ptr);
 };
 
+/*-----------------------
+StompAllocator----> 메모리 오염 잡기 위해!
+-----------------------*/
+
+class StompAllocator
+{
+	enum { PAGE_SIZE = 0x1000 };
+public:
+	static void*	Alloc(int32 size);
+	static void		Release(void* ptr);
+
+};
