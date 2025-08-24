63993cff49fc780e6219d369efdb15d5248a04a0
kwak <b_mule@naver.com>
2025-08-24 15:54:36 +0900

    C++ : Socket Utils Practice

--- C++/Cpp_Server/Server/ServerCore/CorePch.h
+++ C++/Cpp_Server/Server/ServerCore/CorePch.h
@@ -9,8 +9,14 @@
 
 #include <iostream>
 #include <Windows.h>
-#include "Lock.h"
-
-#include "ObjectPool.h"
 using namespace std;
+
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#include "Lock.h"
+#include "ObjectPool.h"
 #include "TypeCast.h"
+#include "Memory.h"
