e09e7fb4ac127157d903c81879da58d798e807d5
Kwak <b_mule@naver.com>
2025-08-31 00:01:04 +0900

    C++ : PacketSession Practice

--- Project/Cpp_Server/Server/ServerCore/Lock.h
+++ Project/Cpp_Server/Server/ServerCore/Lock.h
@@ -1,54 +1,60 @@
 #pragma once
 #include "Types.h"
 
-/*--------------------------
-*         RW SpinLock
-------------------------------*/
+/*----------------
+    RW SpinLock
+-----------------*/
+
+/*--------------------------------------------
+[WWWWWWWW][WWWWWWWW][RRRRRRRR][RRRRRRRR]
+W : WriteFlag (Exclusive Lock Owner ThreadId)
+R : ReadFlag (Shared Lock Count)
+---------------------------------------------*/
+
 class Lock
 {
-	enum : uint32
-	{
-		ACQUIRE_TIMEOUT_TICK = 10000, // 1초
-		MAX_SPIN_COUNT = 5000, // 최대 스핀 횟수,
-		WRITE_THREAD_MASK = 0xFFFF'0000, // 쓰기 스레드 마스크,
-		READ_COUNT_MASK = 0x0000'FFFF, // 읽기 스레드 마스크
-		EMPTY_FLAG = 0x0000'0000, // 비어있는 상태
-	};
+    enum : uint32
+    {
+        ACQUIRE_TIMEOUT_TICK = 10000,
+        MAX_SPIN_COUNT = 5000,
+        WRITE_THREAD_MASK = 0xFFFF'0000,
+        READ_COUNT_MASK = 0x0000'FFFF,
+        EMPTY_FLAG = 0x0000'0000
+    };
 
 public:
-	void WriteLock(const char* name);
-	void WriteUnlock(const char* name);
-	void ReadLock(const char* name);
-	void ReadUnlock(const char* name);
+    void WriteLock(const char* name);
+    void WriteUnlock(const char* name);
+    void ReadLock(const char* name);
+    void ReadUnlock(const char* name);
 
 private:
-	Atomic<uint32> _lockFlag = EMPTY_FLAG; // Lock 상태를 저장하는 변수
-	uint16 _writeCount = 0; // 쓰기 스레드의 개수
+    Atomic<uint32> _lockFlag = EMPTY_FLAG;
+    uint16 _writeCount = 0;
 };
 
-
-/*--------------------------
-*         LockGuard
-------------------------------*/
+/*----------------
+    LockGuards
+-----------------*/
 
 class ReadLockGuard
 {
 public:
-	ReadLockGuard(Lock& lock, const char* name) : _lock(lock), _name(name) {_lock.ReadLock(name);}
-	~ReadLockGuard() { _lock.ReadUnlock(_name); }
+    ReadLockGuard(Lock& lock, const char* name) : _lock(lock), _name(name) { _lock.ReadLock(name); }
+    ~ReadLockGuard() { _lock.ReadUnlock(_name); }
 
 private:
-	Lock& _lock;
-	const char* _name;
+    Lock& _lock;
+    const char* _name;
 };
 
 class WriteLockGuard
 {
 public:
-	WriteLockGuard(Lock& lock, const char* name) : _lock(lock), _name(name){ _lock.WriteLock(name); }
-	~WriteLockGuard() { _lock.WriteUnlock(_name); }
+    WriteLockGuard(Lock& lock, const char* name) : _lock(lock), _name(name) { _lock.WriteLock(name); }
+    ~WriteLockGuard() { _lock.WriteUnlock(_name); }
 
 private:
-	Lock& _lock;
-	const char* _name;
+    Lock& _lock;
+    const char* _name;
 };
