763bc4934478900764f1749294d5293073eaf181
Kwak <b_mule@naver.com>
2025-11-24 19:36:24 +0900

    setting2

--- Project/IocpChatServer/IocpChatServer_LoadTest/ServerCore/CoreGlobal.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Àü¿ª ½Ì±ÛÅæ °´Ã¼ Æ÷ÀÎÅÍµé
-extern class ThreadManager* GThreadManager;
-extern class Memory* GMemory;
-extern class SendBufferManager* GSendBufferManager;
-extern class DeadLockProfiler* GDeadLockProfiler;
-extern class DBConnectionPool* GDBConnectionPool;
